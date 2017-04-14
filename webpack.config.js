51667870399e2488151bf30def062046f385db75
Krzysztof Nowak <krzysztof.michal.nowak@gmail.com>
2017-04-14 10:58:33 +0200

    Skończone iwysłane do BProg - I etap rekrutacji

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+module.exports = {
+    entry: ["bootstrap-loader","./app/app.jsx"],
+    output: {
+        filename: "./js/out.js"
+    },
+    watch: true,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: ['/node_modules/', '/psd/'],
+                loader: 'babel-loader',
+                query: { presets: ['es2015'] }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.css$/,
+                loaders: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|gif|jpg)$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.woff$/,
+                loader: "url-loader?limit=10000&minetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|woff2|eot|svg)$/,
+                loader: "url-loader?limit=10000"
+            }
+        ]
+    }
+}
